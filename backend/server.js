73899d103d2a9b912260b8a9d1730e8d8c5ec178
Rayhan Islam <raihanislamshagor2@gmail.com>
2022-03-13 12:20:41 +0600

    getting data added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+const { errorHandler } = require('./middleware/middleware');
+
+// initialize
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+// error handler
+app.use(errorHandler);
+
+// start/listener server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
